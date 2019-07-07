bf057f6e56a18bda86eb4217fc05581e673050e0
imaglazer <avigayilglazer@gmail.com>
2019-07-07 22:34:24 +0300

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # resources :users
+  # resources :sessions
+
+  root 'welcome#home'
+
+  get '/auth/facebook/callback' => 'sessions#create'
+
+
 end
