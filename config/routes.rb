17aa2758b06dd8f9264604177817d0da5819b75d
Kelsey Low <kklowlow@gmail.com>
2019-07-11 00:24:16 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#home'
+  get '/auth/facebook/callback' => 'sessions#create'
+  
+  resources :users
+  resources :sessions, only: [:new, :create]
 end
